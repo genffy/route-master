b1ea27942a83736b93bc1c3a6ae6ceb38e47a932
genffy <genffyl@gmail.com>
2023-07-17 20:00:29 +0800

    feat: add mapbox

--- packages/editor-app/components/shared/MapEditor.tsx
+++ packages/editor-app/components/shared/MapEditor.tsx
@@ -2,11 +2,40 @@
 
 import React, { useEffect, useRef } from 'react';
 import "./MapEditor.style.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+import mapboxgl from 'mapbox-gl';
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import { MAPBOX_ACCESS_TOKEN } from '@/lib/constants';
+
+import routes from '@/mock/routes.json'
+
 export type Route = {}
 
 type MapEditorProps = {
   routes: Route[];
 }
+
+const token = mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+
+function pointsEqual([aLat, aLng]: [number, number], [bLat, bLng]: [number, number]) {
+  return aLat === bLat && aLng === bLng;
+}
+// demo from https://github.com/apexskier/mapbox-route-editor
+// fetched from server
+// const initialRoute = await (await fetch(url, optoins)).json()
+const initialRoute = {
+  coordinates: [
+    ...routes.map(({ latitude, longitute }) => [longitute, latitude])
+  ],
+  type: "LineString"
+};
+
+const bounds = initialRoute.coordinates.reduce((bounds, coord) => {
+  // @ts-ignore
+  return bounds.extend(coord);
+}, new mapboxgl.LngLatBounds());
+
 
 export default function MapEditor({ routes }: MapEditorProps) {
   const mapRef = useRef(null);
@@ -15,40 +44,166 @@ export default function MapEditor({ routes }: MapEditorProps) {
       if (!mapRef.current) {
         return
       }
-      // FIXME
-      var map = new BMapGL.Map(mapRef.current); // 创建Map实例
-      // FIXME
-      map.centerAndZoom(new BMapGL.Point(lng, lat), 12); // 初始化地图,设置中心点坐标和地图级别
-      map.enableScrollWheelZoom(); // 开启鼠标滚轮
-      let lineLayer = new BMapGL.LineLayer({
-        enablePicked: true,
-        autoSelect: true,
-        pickWidth: 30,
-        pickHeight: 30,
-        selectedColor: 'yellow', // 选中项颜色
-        style: { // 配置样式
-          // borderMask: false,
-          borderColor: 'rgba(27, 142, 236, .6)',
-          borderWeight: 2,
-          strokeWeight: 3,
-          strokeStyle: 'dashed',
-          strokeColor: ['case', ['boolean', ['feature-state', 'picked'], false], '#6704ff', ['match', ['get', 'name'], 'demo1', '#ce4848', 'demo2', '#6704ff', 'demo3', 'blue', '#6704ff']],
+
+      const map = new mapboxgl.Map({
+        container: 'map-content', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [lng, lat], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+
+      const draw = new MapboxDraw({
+        controls: {
+          line_string: true
+        },
+        displayControlsDefault: false,
+        defaultMode: "draw_line_string"
+      });
+
+      map.on("load", async () => {
+        map.fitBounds(bounds, { padding: 20 });
+
+        map.addControl(draw);
+
+        const [editId] = draw.add(initialRoute);
+
+        // store the matched paths we get from the service in a mapbox source
+        map.addSource("rendered-path-source", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: []
+          }
+        });
+
+        // display the matched paths from the service
+        map.addLayer({
+          id: "rendered-path-layer",
+          type: "line",
+          source: "rendered-path-source"
+        });
+
+        // save the last displayed coords so we can tell what changes
+        let lastLineCoords = draw.get(editId).geometry.coordinates;
+
+        // keep track of all matched segments between lines
+        // stored by the index of the first point. if i+1 or i+2 changes, we need to
+        // invalidate the segment
+        // we'll be doing some slightly weird stuff by dyanmically deleting and indexing into
+        // an array. If you debug, the array will be mostly empty, which can be confusing
+        // when this changes, write it to the store (manually right now)
+        let lineMatchLegs = new Array(lastLineCoords.length);
+
+        // get and save a match segment from the service
+        async function updateSegment(i, coords, precision) {
+          const queryCoords = [coords[i], coords[i + 1]]
+            .map(coord => coord.join(","))
+            .join(";");
+
+          const queryRadiuses = [precision, precision].join(";");
+
+          // TODO: Use something like RX.js to create a stream for this. This would
+          // allow me to cancel requests when a new one comes in so I'm always using the
+          // latest data if the user updates too fast for the network to keep up
+          const req = await fetch(
+            `https://api.mapbox.com/matching/v5/mapbox/walking/${queryCoords}?access_token=${token}&geometries=geojson&radiuses=${queryRadiuses}`,
+            { method: "GET" }
+          );
+          const data = await req.json();
+          lineMatchLegs[i] = data.matchings;
         }
+
+        // push saved segments to the mapbox source
+        function updateRenderedPath() {
+          // maybe? improve rendering performance by update/render each segment individually
+          map.getSource("rendered-path-source").setData({
+            type: "FeatureCollection",
+            features: [].concat(
+              ...lineMatchLegs.filter(l => l).map(matches =>
+                matches.map(match => ({
+                  type: "Feature",
+                  properties: {},
+                  geometry: match.geometry
+                }))
+              )
+            )
+          });
+        }
+
+        // full reset, used on init and when we don't know what to do
+        async function resetSegments(coords) {
+          lineMatchLegs = new Array(coords.length);
+          await Promise.all(
+            coords.slice(0, -1).map((_, i) => updateSegment(i, coords, 30))
+          );
+          updateRenderedPath();
+        }
+
+        map.on("draw.update", async ev => {
+          const newCoords = ev.features[0].geometry.coordinates;
+
+          // doesn't account for adding or removing points
+          // just resets if that hppens for now
+          if (lastLineCoords.length === newCoords.length) {
+            let mutatedIndexes;
+            let radiuses;
+
+            // Diff against the last version of the line and only fetch segments for
+            // the point + next and previous coordinates
+            const diffIndex = lastLineCoords.findIndex(
+              (coord, i) => !pointsEqual(newCoords[i], coord)
+            );
+            if (diffIndex < 0) {
+              // just in case
+              return;
+            }
+            mutatedIndexes = [];
+            if (diffIndex > 0) {
+              mutatedIndexes.push(diffIndex - 1);
+            }
+            mutatedIndexes.push(diffIndex);
+            if (diffIndex < newCoords.length - 1) {
+              mutatedIndexes.push(diffIndex + 1);
+            }
+
+            // decrease precision so we get more results. base on zoom, so you can
+            // be more precise when you zoomn in
+            const precision = -Math.pow(map.getZoom(), 2) * (1 / 16) + 50; // TODO: play around with this graph
+
+            // iterate over pairs of path segments (so don't use the last point)
+            // wait for all the segments to update before proceding
+            await Promise.all(
+              mutatedIndexes.slice(0, -1).map(async i => {
+                delete lineMatchLegs[i];
+                await updateSegment(i, newCoords, precision);
+              })
+            );
+          } else {
+            resetSegments(newCoords);
+          }
+
+          updateRenderedPath();
+
+          lastLineCoords = newCoords;
+        });
+
+        map.on("draw.create", ev => {
+          console.log(ev);
+          console.log("Saving to server", ev.features);
+          resetSegments(ev.features[0].geometry.coordinates);
+          // await fetch(url, { body: ev.features, method: "PUT" })
+          // handle errors
+        });
+
+        draw.changeMode("draw_line_string", {
+          featureId: editId,
+          from: initialRoute.coordinates[initialRoute.coordinates.length - 1]
+        });
       });
-      const overlay = new BMapGL.Polyline([
-        new BMapGL.Point(116.42448, 39.92922),
-        new BMapGL.Point(116.31668, 39.92922),
-        new BMapGL.Point(116.38021, 39.88892),
-        new BMapGL.Point(116.42419, 39.90752)
-      ], {
-        strokeColor: '#8055e3',
-        strokeWeight: 5,
-        strokeOpacity: 1
-      });
-      map.addOverlay(overlay);
-      map.addOverlay(lineLayer);
+
+
     }
-    initMap()
+    // initMap()
     if (!navigator.geolocation) {
       console.error(`Your browser doesn't support Geolocation`);
     }
@@ -66,9 +221,6 @@ export default function MapEditor({ routes }: MapEditorProps) {
   console.log(routes)
   return (
     <>
-      <link rel="stylesheet" type="text/css" href="https://api.map.baidu.com/res/webgl/10/bmap.css" />
-      {/*eslint-disable-next-line @next/next/no-sync-scripts*/}
-      <script type="text/javascript" src="https://api.map.baidu.com/getscript?type=webgl&v=1.0&ak=bmsPafLAscGa1GrwRubGzy5iSCIqYSaC&services=&t=20230706173803"></script>
       <div id="map-content" ref={mapRef}></div>
     </>
   )
